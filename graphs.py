9280ab366695d1677d2f0b4f6dc2a1bdf9a3e31b
pncakespoon1 <106548072+pncakespoon1@users.noreply.github.com>
2022-06-02 18:33:33 -0400

    graphs work

--- graphs.py
+++ graphs.py
@@ -2,6 +2,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set_theme(style="darkgrid")
-tips = sns.load_dataset("tips")
-print(tips)
+import pygsheets
+import json
+
+
+path1 = "D:/ResetEfficiency/"
+gc_sheets = pygsheets.authorize(service_file=path1 + "credentials.json")
+
+jsonFile = open(path1 + "runners.json")
+runners = json.load(jsonFile)
+
+
+def get_sheet(sheetname):
+    sh = gc_sheets.open(sheetname)
+    wks = sh[1]
+    return wks
+
+
