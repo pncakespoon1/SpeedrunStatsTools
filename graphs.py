c47f1ab6f541fef9d88bbbc934c6ea869f9ce70d
pncakespoon1 <106548072+pncakespoon1@users.noreply.github.com>
2022-06-02 19:08:49 -0400

    stuffs

--- graphs.py
+++ graphs.py
@@ -19,3 +19,12 @@ def get_sheet(sheetname):
     return wks
 
 
+def get_distribution(sheetname, colname):
+    wks = get_sheet(sheetname)
+    headers = wks.get_row(row=0, returnas='matrix', include_tailing_empty=False)
+    column = wks.get_col(col = headers.index(colname), returnas='matrix', include_tailing_empty=False)
+    column.pop(0)
+    for cell in column:
+        if cell == '':
+            column.remove('')
+    return column
