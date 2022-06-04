9863f0a9bca56741a0baf3bd41e5c2cab9941c95
pncakespoon1 <106548072+pncakespoon1@users.noreply.github.com>
2022-06-04 17:16:18 -0400

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pygsheets
 import json
+from datetime import timedelta
+from datetime import datetime
 
 
 path1 = "D:/ResetEfficiency/"
@@ -22,9 +24,43 @@ def get_sheet(sheetname):
 def get_distribution(sheetname, colname):
     wks = get_sheet(sheetname)
     headers = wks.get_row(row=0, returnas='matrix', include_tailing_empty=False)
-    column = wks.get_col(col=headers.index(colname), returnas='matrix', include_tailing_empty=False)
+    column = wks.get_col(col=headers.index(colname) + 1, returnas='matrix', include_tailing_empty=False)
     column.pop(0)
     for cell in column:
         if cell == '':
             column.remove('')
     return column
+
+def get_nph_and_entryAvg(sheetname, uses_spec_tracker):
+    one_hour = timedelta(hours=1)
+    wks = get_sheet(sheetname)
+    headers = wks.get_row(row=1, returnas='matrix', include_tailing_empty=False)
+    nether_col = wks.get_col(col=headers.index("Nether") + 1, returnas='matrix', include_tailing_empty=True)
+    nether_col.pop(0)
+    rta_col = wks.get_col(col=headers.index("RTA") + 1, returnas='matrix', include_tailing_empty=True)
+    rta_col.pop(0)
+    if uses_spec_tracker:
+        rta_since_prev_col = wks.get_col(col=headers.index("RTA Since Prev") + 1, returnas='matrix', include_tailing_empty=True)
+        rta_since_prev_col.pop(0)
+    nether_count = 0
+    entry_sum = timedelta(seconds=0)
+    rta_sum = timedelta(seconds=0)
+    for i in range(len(nether_col)):
+        nether_cell = nether_col[i]
+        rta_cell = rta_col[i]
+        rta_cell = timedelta(hours=int(rta_cell[0:1]), minutes=int(rta_cell[2:4]), seconds=int(rta_cell[5:7]))
+        if uses_spec_tracker:
+            rta_since_prev_cell = rta_since_prev_col[i]
+            rta_since_prev_cell = timedelta(hours=int(rta_since_prev_cell[0:1]), minutes=int(rta_since_prev_cell[2:4]), seconds=int(rta_since_prev_cell[5:7]))
+            rta_sum += (rta_cell + rta_since_prev_cell)
+        else:
+            rta_sum += rta_cell
+        if nether_cell != '':
+            nether_cell = timedelta(hours=int(nether_cell[0:1]), minutes=int(nether_cell[2:4]), seconds=int(nether_cell[5:7]))
+            nether_count += 1
+            entry_sum += nether_cell
+            rta_sum -= (rta_cell - nether_cell)
+    hours = rta_sum / one_hour
+    nph = nether_count / hours
+    avgEnter = entry_sum / nether_count
+    return nph, avgEnter
